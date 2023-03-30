f05059a2c56715cd8225e9211932d79a403eb79a
Nafchand6 <123442864+Nafchand6@users.noreply.github.com>
2023-03-30 11:01:31 +0400

    Update HomeBox.jsx

---
+++ src/HomeBox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomeBox.css";
+import Widgets from "./Widgets";
+
+
+const HomeBox = () => {
+  return (
+    <div className="Explore">
+      <Widgets/>
+      <div className="HomeBox">
+      </div>
+    </div>
+  );
+};
+
+export default HomeBox; 
