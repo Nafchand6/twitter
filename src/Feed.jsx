847f50efb2d6322f3e7c109c65b451b774f28444
Nafchand6 <123442864+Nafchand6@users.noreply.github.com>
2023-03-30 10:50:22 +0400

    Add files via upload

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,31 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import './Feed.css'
 import TweetBox from './TweetBox'
+import Post from './Post';
+import FlipMove from "react-flip-move";
+
 
 function Feed() {
-    const [posts] = useState([]);
+    const [post, setPost] = useState([]);
+    
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch ("http://localhost:8080/tweet")
+            const jsonResult = await result.json()
+           setPost(jsonResult)
+           
+        }
+        fetchData()
+    }, [])
+    
   
     return (
         <div className = "feed">
             <div className = "feed__header">
-                <h2>Home</h2>
+                <h2>Latest Tweets</h2>
             </div>
 
             <TweetBox />
-            
-                {posts.map(post => (
+           <div style = {{maxHeight: '400px', overflow: 'auto'}}>
+                {post.map(post => (
                 <Post 
-                key = {post.text}
-                displayName = {post.displayName}
-                username = {post.username}
-                verified = {post.verified}
-                text = {post.text}
-                image = {post.image}
-                avatar = {post.avatar}
-            />
+                key={post.id}
+                id={post.displayName}
+                user={post.user}
+                verified={post.verified}
+                text={post.text}
+                avatar={post.avatar}
+                image={post.image}
+              />
             ))}
-              
+             
+             </div>
         </div>
     )
-}
+} 
 export default Feed;
